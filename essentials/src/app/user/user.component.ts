c34265c72523cb1b2db99125758b508e38493361
Emre <emrekurtt1655@gmail.com>
2024-09-05 16:00:32 +0200

    add user component with random number

---
+++ essentials/src/app/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  
+  selectedUser = signal(DUMMY_USERS[randomNumber]);
+
+  // get imagePath() : string {
+  //   return 'assets/users/' + this.selectedUser().avatar
+  // }
+
+  imagePath = computed(() => this.selectedUser().avatar )
+
+  onSelectUser() {
+    const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set( DUMMY_USERS[randomNumber]);
+  }
+
+
+}
