b61b4470181e712c444abb397b5e1636942ea16d
Emre <emrekurtt1655@gmail.com>
2024-09-06 12:47:26 +0200

    extra type check for the event emitter

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -17,8 +17,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) name!: string;
   @Input({ required: true }) avatar!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
