3c3aca135d1d77866483b057f22a495b371092f1
Emre <emrekurtt1655@gmail.com>
2024-09-06 12:42:44 +0200

    add output function instead of decorator

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import {
   EventEmitter,
   Input,
   Output,
+  output
 } from '@angular/core';
 
 @Component({
@@ -16,7 +17,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) name!: string;
   @Input({ required: true }) avatar!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
