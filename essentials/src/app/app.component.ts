55378f5b7befd87f8f89bdcec0c64598947eed08
Emre <emrekurtt1655@gmail.com>
2024-09-09 23:52:13 +0200

    @if @else added

--- essentials/src/app/app.component.ts
+++ essentials/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
     };
   });
 
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
