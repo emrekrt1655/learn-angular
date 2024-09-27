fd6ad2cf629282c9a3552b90ab4594cd293ca01f
Emre <emrekurtt1655@gmail.com>
2024-09-27 23:33:05 +0200

    ng-content

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -3,16 +3,16 @@ import {
   EventEmitter,
   Input,
   Output,
-  output
 } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
