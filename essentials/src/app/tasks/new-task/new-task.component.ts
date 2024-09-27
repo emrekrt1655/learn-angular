6f24efe543385cb9dedae8bd5d875e4a1659e2be
Emre <emrekurtt1655@gmail.com>
2024-09-27 23:24:02 +0200

    submitting the form with ngSubmit

--- essentials/src/app/tasks/new-task/new-task.component.ts
+++ essentials/src/app/tasks/new-task/new-task.component.ts
@@ -1,21 +1,31 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() close: EventEmitter<any> = new EventEmitter<void>();
-  enteredTitle = signal('')
-  enteredSummary= signal('');
-  enteredDate = signal('');
+  @Output() close = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
+
   onCancel() {
-    this.close.emit()
+    this.close.emit();
   }
 
-
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    })
+  }
 }
