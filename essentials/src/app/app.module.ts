61856fc26ad65605d3c09b52b5b8453b97179abe
Emre <emrekurtt1655@gmail.com>
2024-09-28 18:18:56 +0200

    add custom module for shared components

--- essentials/src/app/app.module.ts
+++ essentials/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { TaskComponent } from "./tasks/task/task.component";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, NewTaskComponent, TaskComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, NewTaskComponent, TaskComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, FormsModule, SharedModule]
 })
 export class AppModule {
 
