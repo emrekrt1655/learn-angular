d891523bc7bcde2081ab5afc113354463f4fee3a
Emre <emrekurtt1655@gmail.com>
2024-09-30 13:04:20 +0200

    fix http error

--- projects/todo_app/client/src/app/user/user.component.ts
+++ projects/todo_app/client/src/app/user/user.component.ts
@@ -1,43 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService, User } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent  {
-  // users: User[] = [];
-  // selectedUser?: User;
+export class UserComponent implements OnInit {
+  users: User[] = [];
+  selectedUser?: User;
 
-  // constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
-  // ngOnInit(): void {
-  //   this.loadUsers();
-  // }
+  ngOnInit(): void {
+    this.loadUsers();
+  }
 
-  // loadUsers(): void {
-  //   this.userService.getUsers().subscribe({
-  //     next: (users) => {
-  //       this.users = users;
-  //     },
-  //     error: (err) => {
-  //       console.error('Error fetching users', err);
-  //     },
-  //   });
-  // }
+  loadUsers(): void {
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        console.log(users)
+        this.users = users;
+      },
+      error: (err) => {
+        console.error('Error fetching users', err);
+      },
+    });
+  }
 
-  // getUser(id: number): void {
-  //   this.userService.getUser(id).subscribe({
-  //     next: (user) => {
-  //       this.selectedUser = user;
-  //     },
-  //     error: (err) => {
-  //       console.error('Error fetching user', err);
-  //     },
-  //   });
-  // }
+  getUser(id: number): void {
+    this.userService.getUser(id).subscribe({
+      next: (user) => {
+        this.selectedUser = user;
+      },
+      error: (err) => {
+        console.error('Error fetching user', err);
+      },
+    });
+  }
 
 }
--- projects/todo_app/client/src/app/user/user.service.ts
+++ projects/todo_app/client/src/app/user/user.service.ts
@@ -18,23 +18,23 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  // getUsers(): Observable<User[]> {
-  //   return this.http.get<User[]>(this.apiUrl);
-  // }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 
-  // getUser(id: number): Observable<User> {
-  //   return this.http.get<User>(`${this.apiUrl}/${id}`);
-  // }
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
 
-  // createUser(user: User): Observable<User> {
-  //   return this.http.post<User>(this.apiUrl, user);
-  // }
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
 
-  // updateUser(user: User): Observable<User> {
-  //   return this.http.put<User>(`${this.apiUrl}/${user.id}`, user);
-  // }
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${user.id}`, user);
+  }
 
-  // deleteUser(id: number): Observable<void> {
-  //   return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  // }
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
